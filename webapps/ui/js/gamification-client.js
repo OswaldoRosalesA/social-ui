c5326dc365509a5412c76d6ad3e2acb58cb582cb
Oswaldo Rosales <oswaldo.rosales.a@gmail.com>
2020-02-22 15:52:39 -0600

    Feature: Chapter 5 - UI

---
+++ webapps/ui/js/gamification-client.js
@@ -0,0 +1,38 @@
+const gamificationHost = "http://localhost:8001";
+
+function updateLeaderBoard() {
+    $.ajax({
+        url: gamificationHost + "/leaders"
+    }).then(function(data) {
+        $('#leaderboard-body').empty();
+        data.forEach(function(row) {
+            $('#leaderboard-body').append('<tr><td>' + row.userId + '</td>' +
+                '<td>' + row.totalScore + '</td>');
+        });
+    });
+}
+
+function updateStats(userId) {
+    $.ajax({
+        url: gamificationHost + "/stats?userId=" + userId,
+        success: function(data) {
+            $('#stats-div').show();
+            $('#stats-user-id').empty().append(userId);
+            $('#stats-score').empty().append(data.score);
+            $('#stats-badges').empty().append(data.badges.join());
+        },
+        error: function(data) {
+            $('#stats-div').show();
+            $('#stats-user-id').empty().append(userId);
+            $('#stats-score').empty().append(0);
+            $('#stats-badges').empty();
+        }
+    });
+}
+
+$(document).ready(function() {
+    updateLeaderBoard();
+    $("#refresh-leaderboard").click(function( event ) {
+        updateLeaderBoard();
+    });
+});
